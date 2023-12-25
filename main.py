bac433ba8de838998433960abc9ad0525ff21653
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-24 20:48:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import logging
 import json
 
 app = FastAPI()
+with open('biliEmote.json') as f:
+    emotes = json.load(f)
 
 
 @app.get("/get-emote/")
-async def get_emote(id: str = None, mid: str = None):
+async def get_emote(eid: str = None, mid: str = None):
     try:
-        with open('biliEmote.json') as f:
-            emotes = json.load(f)
         for emote in emotes['data']['all_packages']:
-            if id == emote['id'] or mid == emote['meta']['item_id']:
+            if eid == emote['id'] or mid == emote['meta']['item_id']:
                 return emote
         raise Exception('emote not found.')
     except:
