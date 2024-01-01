04f169f4fb01875f53f96c3dc08880bffd8a98ab
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2023-12-31 16:57:05 -0800

    fix: req timeout time

--- emotescraper.py
+++ emotescraper.py
@@ -3,6 +3,6 @@ import sys
 import json
 
 if __name__ == '__main__':
-    res = requests.get(sys.argv[1], timeout=10)
+    res = requests.get(sys.argv[1], timeout=60)
     with open('biliEmote.json', 'w', encoding='utf-8') as f:
         json.dump(res.json(), f)
