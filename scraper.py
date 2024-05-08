a3e8ccb1584759a4c44259b7f59e82d453c0e102
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-05-08 22:34:34 +0000

    chore: code clean up

--- scraper.py
+++ scraper.py
@@ -16,14 +16,6 @@ https://api.bilibili.com/x/vas/dlc_act/act/item/list?act_id={藏品id}
 '''
 BASIC_API = 'https://api.bilibili.com/x/vas/dlc_act/act/basic?act_id={id}'
 
-
-def save_last_scraped(scraped: str):
-    logging.debug('saving %s to disk.', scraped)
-    with open('scraped.point', 'w', encoding='utf-8') as f:
-        json.dump(str(int(scraped) - 1), f)
-    sys.exit(0)
-
-
 def write_list():
     with open('list.md', 'w', encoding='utf-8') as f:
         for jsondata in sorted(glob.glob('data/*.json')):
@@ -52,18 +44,7 @@ def scrape_id(last_scraped: str):
     if jsoned['code'] != 0:
         logging.warning(
             '%s errored out: %s.', last_scraped, json.dumps(jsoned))
-        if jsoned['message'] == "活动ID不存在":
-            logging.info('%s DNE. terminating program?', last_scraped)
-            last_scraped = str(int(last_scraped) + 1)
-            time.sleep(1)
-            # save_last_scraped(last_scraped)
-            return 1
-        else:
-            last_scraped = str(int(last_scraped) + 1)
-            logging.info(
-                '%s errors but message is not DNE. program will attempt to continue', last_scraped)
-            time.sleep(1)
-            return 1
+        return 1
     logging.info(
         'scaped %s to be %s', last_scraped, jsoned["data"]["act_title"])
     data['basic'] = jsoned['data']
@@ -73,25 +54,6 @@ def scrape_id(last_scraped: str):
     with open(join('data', f'BILINFT_{last_scraped}.json'), 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
     return 0
-
-
-def old_scrape():
-    try:
-        with open('scraped.point', encoding='utf-8') as f:
-            last_scraped = json.load(f)
-    except Exception as e:
-        logging.error(e)
-        last_scraped = '100'
-
-    forcequit = 0
-    saved_last_scraped = last_scraped
-    while True:
-        if forcequit > 10:
-            write_list()
-            save_last_scraped(saved_last_scraped)
-        forcequit += scrape_id(last_scraped)
-        saved_last_scraped = last_scraped = str(int(last_scraped) + 1)
-
 
 if __name__ == '__main__':
     import argparse
@@ -112,4 +74,3 @@ if __name__ == '__main__':
             scrape_id(nft_id)
 
     write_list()
-    # old_scrape()
